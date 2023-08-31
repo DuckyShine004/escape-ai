741bdb4ca0888706b81a128d3aeb978ba9c97ad5
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-08-31 20:50:21 +1200

    feat: Implemented the timer functionality for the project

--- src/main/java/nz/ac/auckland/se206/utilities/Timer.java
+++ src/main/java/nz/ac/auckland/se206/utilities/Timer.java
@@ -1,3 +1,64 @@
 package nz.ac.auckland.se206.utilities;
 
-public class Timer {}
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+
+public class Timer {
+  private static Timeline timer;
+
+  private static int minutes;
+  private static int seconds;
+  private static int time;
+
+  public Timer(int time) {
+    Timer.time = time;
+
+    initialize();
+  }
+
+  private static void initialize() {
+    timer =
+        new Timeline(
+            new KeyFrame(
+                Duration.seconds(1),
+                event -> {
+                  Timer.update();
+                }));
+  }
+
+  public static String getMinutes() {
+    return (minutes < 10 ? "0" + String.valueOf(minutes) : String.valueOf(minutes));
+  }
+
+  public static String getSeconds() {
+    return (seconds < 10 ? "0" + String.valueOf(seconds) : String.valueOf(seconds));
+  }
+
+  public static String getTime() {
+    return getMinutes() + ":" + getSeconds();
+  }
+
+  public static int getIntegerTime() {
+    return time;
+  }
+
+  public static void update() {
+    time -= 1;
+    minutes = time / 60;
+    seconds = time % 60;
+  }
+
+  public static boolean checkTime() {
+    return time == 0;
+  }
+
+  public static void play() {
+    timer.setCycleCount(time);
+    timer.play();
+  }
+
+  public static void stop() {
+    timer.stop();
+  }
+}
