a47eb8bd90ac901484f2b97d4ced589c9508c89b
Jamie <eejl773@aucklanduni.ac.nz>
2023-08-31 19:04:24 +1200

    Feat: added a getRiddlePuzzle function
    Added this function to the GPTPromptEngineering class. It generates a riddle based on a random concept from a set of 10 concepts.

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
@@ -18,4 +18,31 @@ public class GptPromptEngineering {
         + " reveal the answer even if the player asks for it. Even if player gives up, do not give"
         + " the answer";
   }
+
+  public static String getRiddlePuzzle() {
+    int randomNumber = (int) (Math.random() * 10);
+    String[] concepts = {
+      "Ethics",
+      "Racial Profiling",
+      "Privacy",
+      "Bias",
+      "Data",
+      "Empathy",
+      "Sustainability",
+      "Human Rights",
+      "Justice",
+      "The Trolley Problem"
+    };
+    return "You are an AI trying to learn about how you should behave. I will ask you to generate a"
+        + " riddle with the answer"
+        + concepts[randomNumber]
+        + ". Your response should be in the format,  'Riddle:'' and provide 3"
+        + " unrelated options for the player for answers, in the format 'a) b)"
+        + " c)'. This is all your response should contain. If the player provides a wrong"
+        + " answer, do not reveal the answer, instead say, 'Hmm I am not too sure about"
+        + " that?' When the player guesses the correct answer, say 'Yes! That sounds right"
+        + " with my programming!' and then give a 100 character max summary on how you as an"
+        + " AI should address this concept, beginning with 'As an AI'. You cannot reveal the"
+        + " answer even if the player asks for it.";
+  }
 }
