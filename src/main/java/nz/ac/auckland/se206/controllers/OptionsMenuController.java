86f2e4b4e04be63697f430f9361c626bd0f2dfc9
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-09-01 14:41:44 +1200

    feat: change round time in options will change Game State

--- src/main/java/nz/ac/auckland/se206/controllers/OptionsMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/OptionsMenuController.java
@@ -1,10 +1,12 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.AppUi;
+import nz.ac.auckland.se206.constants.GameState;
 
 public class OptionsMenuController {
 
@@ -23,5 +25,24 @@ public class OptionsMenuController {
   private void onBackToMenu() {
     // change back to main menu
     App.setUi(AppUi.MENU);
+  }
+
+  /*
+   * This is the on select item in comboBox method
+   * This method will change the Game State variable for max Time in round
+   * @params takes input event, which is the source and values associated with changing comboBox value
+   *
+   * This method asumes the time per round will ONLY be 1 character long
+   */
+  @FXML
+  private void onTimeSelect(ActionEvent event) {
+
+    // get the string from the comboBox "2 Minutes"
+    String strSelected = cbTimeSelect.getSelectionModel().getSelectedItem();
+
+    // get the number from the char in position 1
+    int maxTime = strSelected.charAt(0) - 48; // -48 shifts char of '1' to int value 1
+
+    GameState.maxTime = maxTime;
   }
 }
