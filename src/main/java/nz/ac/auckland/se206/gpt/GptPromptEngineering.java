cb97ae3ffaab09f7c1fe5ca13f4a2ce36e9fa2c3
Jamie <eejl773@aucklanduni.ac.nz>
2023-09-04 15:35:56 +1200

    Refactor: updated prompt engineering to reduce chance of generating half finished riddles

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
@@ -21,13 +21,13 @@ public class GptPromptEngineering {
 
   public static String getRiddlePuzzle(String concept) {
     return "You are an AI trying to learn about how you should behave. I will ask you to generate a"
-        + " riddle with the answer"
+        + " riddle up to 150 characters long with the answer"
         + concept
         + ". Your response should start with 'Riddle:', and end the riddle with a '^'. Only after the '^'"
         + " is printed, list three options, each between a set of {} to the player to choose"
         + " from, one of which is "
         + concept
-        + " and the other two are words that are not related to "
+        + " and the other two are words that are unrelated to "
         + concept
         + ". Do not precede the options with anything. This is all your response should contain. If"
         + " the player provides a wrong answer, do not reveal the answer, instead say, 'Hmm I am"
