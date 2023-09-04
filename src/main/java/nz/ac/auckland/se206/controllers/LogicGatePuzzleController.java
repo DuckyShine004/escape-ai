322af97c307eb21e49346489ebfc7870c01bc603
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-09-04 14:29:31 +1200

    feat: added logic gate puzzle layout scene

--- src/main/java/nz/ac/auckland/se206/controllers/LogicGatePuzzleController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LogicGatePuzzleController.java
@@ -2,11 +2,15 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class LogicGatePuzzleController {
   @FXML private Label lblTimer;
+
+  @FXML private ImageView imgGate1;
+  @FXML private ImageView imgTable1;
 
   /*
    * This method changes the scene back to the Breaker Room
