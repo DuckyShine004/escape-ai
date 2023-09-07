8ca761e87008116f2bd7500af2d22bf019b402ae
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-09-07 16:42:06 +1200

    docs: Created Necessary Files to Start Working on Decryption Puzzle

---
+++ src/main/java/nz/ac/auckland/se206/controllers/DecryptionPuzzleController.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+
+public class DecryptionPuzzleController {
+  /** Initializes the decryption puzzle. */
+  @FXML
+  private void initialize() {}
+}
