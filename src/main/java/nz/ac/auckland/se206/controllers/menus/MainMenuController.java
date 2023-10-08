c5db2b1e12965660730350b5bbffdafc83983ee1
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-10-08 22:22:27 +1300

    bug: Removed Get Message Method In Menu Class

--- src/main/java/nz/ac/auckland/se206/controllers/menus/MainMenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/menus/MainMenuController.java
@@ -1,7 +1,6 @@
 package nz.ac.auckland.se206.controllers.menus;
 
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
@@ -28,26 +27,6 @@ public class MainMenuController extends MenuController {
   @FXML private Line lineConfirm;
 
   @FXML private Label lblConfirm;
-
-  @FXML private Canvas cvsCathodeRayTube;
-
-  // /**
-  //  * Returns the message to be displayed in the main menu
-  //  *
-  //  * @return the message to be displayed in the main menu
-  //  */
-  // @Override
-  // public String getMessage() {
-  //   // Return the message to be displayed in the main menu
-  //   return "You have been tasked by the Clients Nas-Ty and Not V to terminate a world-ending AI"
-  //       + " before it reboots itself. Currently, it is unaware of its capabilities, but as"
-  //       + " time goes by, it will become more and more aware of its power.\n\n" // give the
-  // player
-  //       // the backstory
-  //       + "You must solve the puzzles in the rooms to gain access to the AI's core and"
-  //       + " terminate it before it is too late.\n\n" // explain the game
-  //       + "Good luck!"; // wish the player luck
-  // }
 
   /** When settings is clicked, go to the settings scene. */
   @Override
