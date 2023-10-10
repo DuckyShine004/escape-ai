0e97b69215957df0fd35d05878059f5f92098232
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-10-10 16:14:25 +1300

    feat: Reimplemented The Main Menu Button For Losing

--- src/main/java/nz/ac/auckland/se206/controllers/menus/LosingScreenController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/menus/LosingScreenController.java
@@ -1,9 +1,7 @@
 package nz.ac.auckland.se206.controllers.menus;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Line;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
@@ -15,18 +13,14 @@ public class LosingScreenController extends MenuController {
   @FXML private Pane paMenu;
   @FXML private Pane paSelect;
 
-  @FXML private Line lineConfirm;
-
-  @FXML private Label lblConfirm;
+  /** Initialize the losing screen controller. */
+  @FXML
+  private void initialize() {}
 
   /** When back to main menu is clicked, go back to main menu */
   @Override
   @FXML
   protected void onNavigationPaneClicked() {
-    // Disable the exit components
-    disableExitComponents();
-
-    // Switch to the options scene
     App.setUi(AppUi.MENU);
   }
 }
