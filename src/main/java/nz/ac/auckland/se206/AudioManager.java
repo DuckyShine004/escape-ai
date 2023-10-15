7545457c413a7c1326ce082e4ce01d304c64df5a
Gallon Zhou <123701457+DuckyShine004@users.noreply.github.com>
2023-10-15 18:28:38 +1300

    docs: Added java Docs For Audio Manager

--- src/main/java/nz/ac/auckland/se206/AudioManager.java
+++ src/main/java/nz/ac/auckland/se206/AudioManager.java
@@ -20,15 +20,34 @@ public class AudioManager {
 
   private static HashMap<Clip, AudioClip> audioMap = new HashMap<>();
 
+  /**
+   * Add audio to the audio map.
+   *
+   * @param clip the clip
+   * @param path the path to the sound file
+   */
   public static void addAudio(Clip clip, String path) {
     AudioClip audio = new AudioClip(App.class.getResource(path).toString());
     audioMap.put(clip, audio);
   }
 
+  /**
+   * Loads an audio clip and plays it.
+   *
+   * @param clip the clip
+   */
   public static void loadAudio(Clip clip) {
     audioMap.get(clip).play();
   }
 
+  /**
+   * Initialize a looping sound effect.
+   *
+   * @param timeline the timeline to be set
+   * @param clip the clip
+   * @param delay the delay
+   * @return the initialized timeline
+   */
   private static Timeline getTimedSound(Timeline timeline, Clip clip, double delay) {
     timeline =
         new Timeline(
@@ -41,10 +60,16 @@ public class AudioManager {
     return timeline;
   }
 
+  /**
+   * Check if the dialogue sound effect is playing.
+   *
+   * @return boolean value based on whether dialgoue is playing
+   */
   public static boolean isDialoguePlaying() {
     return dialogue.getStatus() == Timeline.Status.PAUSED;
   }
 
+  /** Play the heart beat sound effect. */
   public static void playHeartBeat() {
     // Initialize the heart beat sound effect
     heartBeat = getTimedSound(heartBeat, Clip.HEART_BEAT, 2);
@@ -54,6 +79,7 @@ public class AudioManager {
     heartBeat.play();
   }
 
+  /** Play the dialogue sound effect. */
   public static void playDialogue() {
     dialogue = getTimedSound(dialogue, Clip.DIALOGUE, 0.1);
 
@@ -62,18 +88,22 @@ public class AudioManager {
     dialogue.play();
   }
 
+  /** Pause the dialogue sound effect. */
   public static void pauseDialogue() {
     dialogue.pause();
   }
 
+  /** Resume playing the dialogue sound effect. */
   public static void resumeDialogue() {
     dialogue.play();
   }
 
+  /** Stop the heart beat sound effect. */
   public static void stopHeartBeat() {
     heartBeat.stop();
   }
 
+  /** Completely stop playing the dialogue sound effect. */
   public static void stopDialogue() {
     dialogue.stop();
   }
